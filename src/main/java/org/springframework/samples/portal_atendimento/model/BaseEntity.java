97c73a808b5fd991fb346ad963ed0a63092f303c
farid-tekton <156039140+farid-tekton@users.noreply.github.com>
2025-02-25 20:33:34 -0300

    ajuste mvn spring-javaformat:apply

--- src/main/java/org/springframework/samples/portal_atendimento/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/portal_atendimento/model/BaseEntity.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- 
+
  */
 package org.springframework.samples.portal_atendimento.model;
 
@@ -39,7 +39,7 @@ public class BaseEntity implements Serializable {
 
 	public Integer getId() {
 		return id;
-	} 
+	}
 
 	public void setId(Integer id) {
 		this.id = id;
